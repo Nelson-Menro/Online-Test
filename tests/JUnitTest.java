2376fef63e7406f1cfb5e1cc62afa2f4a87c7da4
Nelson Mendez <nelson.mendez7@gmail.com>
2021-08-12 11:30:26 -0400

    removed dead code

--- tests/JUnitTest.java
+++ tests/JUnitTest.java
@@ -41,40 +41,4 @@ public class JUnitTest {
 		System.out.println(answer);
 		//assertTrue(TestingSupport.isResultCorrect(testName, answer.toString(), true));
 	}
-    /*
-    @Test
-	public void pub01TrueFalse() {
-		String testName = new Object() {
-		}.getClass().getEnclosingMethod().getName();
-		
-		StringBuffer answer = new StringBuffer();
-		SystemManager manager = new SystemManager();
-		manager.addExam(10, "Midterm");
-		manager.addTrueFalseQuestion(10, 1, "Abstract classes cannot have constructors.", 2, false);
-		manager.addTrueFalseQuestion(10, 2, "The equals method returns a boolean.", 4, true);
-		manager.addTrueFalseQuestion(10, 3, "Identifiers can start with numbers.", 3, false);
-		answer.append(manager.getKey(10));
-		String studentName = "Smith,John";
-		manager.addStudent(studentName);
-		manager.answerTrueFalseQuestion(studentName, 10, 1, false);
-		manager.answerTrueFalseQuestion(studentName, 10, 2, true);
-		manager.answerTrueFalseQuestion(studentName, 10, 3, false);
-		answer.append("Exam score for " + studentName + " " + manager.getExamScore(studentName, 10));
-		studentName = "Peterson,Rose";
-		manager.addStudent(studentName);
-		manager.answerTrueFalseQuestion(studentName, 10, 1, false);
-		manager.answerTrueFalseQuestion(studentName, 10, 2, false);
-		manager.answerTrueFalseQuestion(studentName, 10, 3, false);
-		answer.append("\nExam score for " + studentName + " " + manager.getExamScore(studentName, 10));
-		
-		studentName = "Sanders,Linda";
-		manager.addStudent(studentName);
-		manager.answerTrueFalseQuestion(studentName, 10, 1, true);
-		manager.answerTrueFalseQuestion(studentName, 10, 2, false);
-		manager.answerTrueFalseQuestion(studentName, 10, 3, true);
-		answer.append("\nExam score for " + studentName + " " + manager.getExamScore(studentName, 10));
-		assertTrue(TestingSupport.isResultCorrect(testName, answer.toString(), true));
-	}
-
-*/
 }
